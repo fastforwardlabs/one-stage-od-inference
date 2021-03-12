910f54f5ad6ffd943577231f0e455fa30da10ee7
andrewrreed <areed1242@gmail.com>
2021-03-12 09:42:13 -0500

    Update app_utils.py

--- src/app_utils.py
+++ src/app_utils.py
@@ -1,6 +1,8 @@
 import os
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 from src.retinanet import retinanet_resnet50_fpn
 
@@ -14,7 +16,7 @@ PRESET_IMAGES = {
 APP_PAGES = [
     "0. Welcome",
     "1. Feature Extraction",
-    "2. Anchor Box Generation",
+    "2. Region Proposal Network",
     "3. Post Processing",
 ]
 
@@ -155,5 +157,100 @@ def get_feature_map_plot(model):
 
     samples = sample_feature_maps(model.viz_artifacts["features"], 7)
     fig = plot_feature_samples(samples)
+
+    return fig
+
+
+def plot_anchors(image, boxes, sample):
+    """
+    Overlay anchor boxes on the original image
+
+    Args:
+        image - PIL image as RGB format
+        boxes - Tensor(N,4) of all anchor box specs
+        sample - percentage of anchorboxes to randomly select for visualization
+
+    """
+
+    fig, ax = plt.subplots(1, figsize=(10, 10))
+    ax.imshow(image)
+
+    num_samples = int(sample * boxes.shape[0])
+    sample_idxs = torch.randint(low=0, high=boxes.shape[0], size=(num_samples,))
+    boxes = boxes[sample_idxs]
+
+    for i, box in enumerate(boxes):
+
+        x, y, width, height = convert_bb_spec(*box)
+        top = y + height
+
+        patch = patches.Rectangle(
+            (x, y),
+            width,
+            height,
+            edgecolor="red",
+            linewidth=1,
+            facecolor="none",
+        )
+        ax.add_patch(patch)
+
+    plt.axis("off")
+
+    return fig
+
+
+def plot_pyramid_level_anchors(
+    pyramid_level_idx, img, image_size, strides, cell_anchors, pred_boxes
+):
+    """
+    This function overlays a full set of anchor boxes (all aspect ratios and sizes) on a given image
+    centered atop each object detected in the image. Specifiying the pyramid level allows you to
+    visualize the size of the anchor boxes relative to the feature map resolution (shown by grid size)
+
+    Args:
+        pyramide_level_index (int)
+        img (PIL.Image.Image)
+        image_size (torch.Size)
+        strides (List[List[torch.Tensor]])
+        cell_anchors (List[torch.Tensor])
+        pred_boxes (torch.Tensor)
+
+    Returns:
+        fig - matplotlib figure
+
+    """
+
+    figsize = [round(i / 100) for i in image_size]
+    fig, ax = plt.subplots(1, figsize=(figsize[::-1]))
+    ax.grid(True)
+    ax.set_xticks(np.arange(0, image_size[1], strides[pyramid_level_idx][1]))
+    ax.set_yticks(np.arange(0, image_size[0], strides[pyramid_level_idx][0]))
+    ax.axes.xaxis.set_ticklabels([])
+    ax.axes.yaxis.set_ticklabels([])
+    ax.title(f"P{pyramid_level_idx+3} - ")
+    ax.imshow(img.resize(image_size[::-1]), aspect="auto", alpha=0.4)
+
+    box_centers = [
+        ((box[2] - box[0]) / 2 + box[0], (box[3] - box[1]) / 2 + box[1])
+        for box in pred_boxes.tolist()
+    ]
+
+    for box_center in box_centers:
+        for i, box in enumerate(cell_anchors[pyramid_level_idx]):
+            x, y, width, height = convert_bb_spec(*box)
+
+            x_offset = box_center[0]
+            y_offset = box_center[1]
+
+            patch = patches.Rectangle(
+                (x + x_offset, y + y_offset),
+                width,
+                height,
+                alpha=0.5,
+                edgecolor="red",
+                linewidth=2,
+                facecolor="none",
+            )
+            ax.add_patch(patch)
 
     return fig
