91edfcad741cd6f873cec068cd95b6c47160c9eb
andrewrreed <areed1242@gmail.com>
2021-03-16 09:27:50 -0400

    Update fpn.py

--- app/fpn.py
+++ app/fpn.py
@@ -15,14 +15,14 @@ def feature_extraction(session_state):
     https://jonathan-hui.medium.com/understanding-feature-pyramid-networks-for-object-detection-fpn-45b227b9106c
 
     """
-    st.title("Multi-scale Feature Extraction")
+    st.title("1. Multi-scale Feature Extraction")
     st.write(
         "Feature extraction is central to any computer vision pipeline and is traditionally performed using deep networks of stacked convolutional layers \
         that refine raw images into a semantically rich, low dimensional representations. This approach is transferable to object detection, however it must \
-        be augmented to maintain scale invariant representations because in the real-world, objects from the same class can exist at a wide range of sizes depending on their depth in an image. \
+        be augmented to maintain scale invariant image representations because in the real-world, objects from the same class can exist at a wide range of sizes depending on their depth in an image. \
         Recognizing objects a varying scales, particularly small objects is a fundamental challenge in object detection. \
         RetinaNet uses a [Feature Pyramid Network (FPN)](https://arxiv.org/pdf/1612.03144.pdf) to solve this problem by extracting feature maps from multiple \
-        levels from a [ResNet](https://arxiv.org/pdf/1512.03385.pdf) backbone."
+        levels of a [ResNet](https://arxiv.org/pdf/1512.03385.pdf) backbone."
     )
 
     with st.beta_expander("How Do FPNs Work?", expanded=False):
@@ -37,8 +37,8 @@ def feature_extraction(session_state):
         st.write(
             "**a. Bottom-up Pathway:** An FPN can be constructed from any deep CNN, but RetinaNet chooses a ResNet architecture. In ResNet, convolutional layers are grouped together \
                 into stages by their output size. The bottom-up pathway of the FPN simply extracts a feature map as the output from the last layer of each stage called a pyramid level. \
-                RetinaNet constructs a pyramid with levels P$_3$ - P$_7$, where P$_l$ indicates pyramid level and has reslution 2$^l$ lower than the input. P$_0$ - P$_2$ are omitted from the FPN \
-                due to the high dimensional impact on memory and computation."
+                RetinaNet constructs a pyramid with levels P$_3$ - P$_7$, where P$_l$ indicates pyramid level and has resolution 2$^l$ lower than the input image. Each pyramid level holds 256 channels, \
+                and P$_0$ - P$_2$ are omitted from the FPN because their high dimensionality has substantial impact on memory and computation speed."
         )
         st.write(
             "**b. Top-down Pathway** The top-down pathway regenerates higher resolution features by upsampling spatially coarser, but semantically stronger feature maps from higher pyramid levels. \
@@ -58,6 +58,7 @@ def feature_extraction(session_state):
             maintain higher resolution, but semantically weaker attributes which are useful for detecting small objects. In contrast, features from the seventh level \
             (P7) hold much lower resolution, but semantically stronger attributes, making them effective for identifying larger objects."
         )
-        st.pyplot(session_state.data_artifacts["feature_map_fig"])
+        # st.pyplot(session_state.data_artifacts["feature_map_fig"])
+        st.image(session_state.img_paths["fpn"])
 
-    return session_state
+    return
